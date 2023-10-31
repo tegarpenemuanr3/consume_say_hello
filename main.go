e351be5ca979fc0b32d46357912117f3291cd222
tegar <tegar@aksaramaya.com>
2023-10-31 09:41:21 +0700

    Update ke v2.0.0

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/tegarpenemuanr3/go_say_hello"
+	go_say_hello "github.com/tegarpenemuanr3/go_say_hello/v2"
 )
 
 func main() {
-	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(go_say_hello.SayHello("Tegar Penemuan"))
 }
