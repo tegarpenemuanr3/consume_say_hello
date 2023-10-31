607a5f373a0f668b960cccc15d692abf91c42743
tegar <tegar@aksaramaya.com>
2023-10-31 09:38:13 +0700

    Consume say-hello v1.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tegarpenemuanr3/consume_say_hello
+
+go 1.21.3
+
+require github.com/tegarpenemuanr3/go_say_hello v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/tegarpenemuanr3/go_say_hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
